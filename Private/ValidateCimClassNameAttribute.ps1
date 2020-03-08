ad9520afce2a15d166e90ed449162c8a9b93d71c
saladproblems <dmcdonough@outlook.com>
2020-03-07 22:47:24 -0600

    ValidateCimClass help added

--- Private/ValidateCimClassNameAttribute.ps1
+++ Private/ValidateCimClassNameAttribute.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+    Class for validating cim Class type names
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    function test-validator {
+        [cmdletbinding()]
+        param(
+            [ValidateCimClass('Win32_Bios')]
+            [ciminstance[]]$cimInstance
+        )
+
+        $cimInstance    
+    }
+    test-validator -ciminstance (Get-CimInstance Win32_Bios -computername localhost,localhost)
+
+    Use the validator in a function to verify the cim class is the correct type 
+.NOTES
+    The goal here is to clean up code in some of the module's functions by using this validator to confirm we aren't performing the actions on the wrong type of objects
+#>
 class ValidateCimClass : System.Management.Automation.ValidateEnumeratedArgumentsAttribute {
 
     [string]$PropertyName    
@@ -12,16 +33,3 @@ class ValidateCimClass : System.Management.Automation.ValidateEnumeratedArgument
         }
     }
 }
-<#
-function test-validator {
-    [cmdletbinding()]
-    param(
-        [ValidateCimClass('Win32_Bios')]
-        [ciminstance[]]$cimInstance
-    )
-
-    $cimInstance    
-}
-
-test-validator -ciminstance (Get-CimInstance Win32_Bios -computername localhost,localhost)
-#>
