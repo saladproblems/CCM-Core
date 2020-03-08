6a585b1786f227f22880e6abbe796e2a06f4b2a9
saladproblems <dmcdonough@outlook.com>
2020-03-08 09:09:10 -0500

    ValidateCimClass can take a delimited string

--- Private/ValidateCimClassNameAttribute.ps1
+++ Private/ValidateCimClassNameAttribute.ps1
@@ -4,18 +4,31 @@
 .DESCRIPTION
     Long description
 .EXAMPLE
+function test-validator {
+    [cmdletbinding()]
+    param(
+        [ValidateCimClass('Win32_OperatingSystem')]
+        [ciminstance[]]$cimInstance
+    )
+
+    $cimInstance    
+}
+test-validator -ciminstance (Get-CimInstance Win32_operatingsystem -computername localhost,localhost)
+
+Use the validator in a function to verify the cim class is the correct type 
+.EXAMPLE
     function test-validator {
         [cmdletbinding()]
         param(
-            [ValidateCimClass('Win32_Bios')]
+            [ValidateCimClass('Win32_Bios,Win32_OperatingSystem')]
             [ciminstance[]]$cimInstance
         )
 
         $cimInstance    
     }
-    test-validator -ciminstance (Get-CimInstance Win32_Bios -computername localhost,localhost)
+    test-validator -ciminstance (Get-CimInstance Win32_operatingsystem -computername localhost,localhost)
 
-    Use the validator in a function to verify the cim class is the correct type 
+    Use a comma delimited list of cimclass names 
 .NOTES
     The goal here is to clean up code in some of the module's functions by using this validator to confirm we aren't performing the actions on the wrong type of objects
 #>
@@ -23,12 +36,12 @@ class ValidateCimClass : System.Management.Automation.ValidateEnumeratedArgument
 
     [string]$PropertyName    
 
-    ValidateCimClass([string]$PropertyName) {
-        $this.PropertyName = $PropertyName
+    ValidateCimClass([string[]]$PropertyName) {
+        $this.PropertyName = $PropertyName -split ','
     }
 
     [void]ValidateElement($Element) {
-        if ([string]$Element.CimClass.CimClassName -ne $this.PropertyName) {
+        if ([string]$Element.CimClass.CimClassName -in $this.PropertyName) {
             throw ('{0} != {1}' -f $this.PropertyName, $Element.CimClass.CimClassName)
         }
     }
